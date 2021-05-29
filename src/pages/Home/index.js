8066714b37d60143beabdec6dac3924c0ac94fd5
ManishRaghavan <manish.mahendran9499@gmail.com>
2021-05-29 23:54:51 +0530

    completed functionalities  for person page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -142,6 +142,6 @@ function HomePage() {
       )}
     </div>
   );
-}
+} 
 
 export default HomePage;
--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './index.css';
-
+import { useHistory } from "react-router-dom";
 function NotFound() {
+  
+  const history = useHistory()
+  const handleRedirect =()=>{
+    history.push("/")
+  }
+
   return (
     <div className="not-found">
       <h2>This is not the page you are looking for</h2>
